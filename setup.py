9b9817c74ad67bfca21f6aa14b30a49d22773d0d
Ravi0Dubey <ravi0dubey@gmail.com>
2024-03-11 08:47:41 -0400

    changing dockerfile

--- setup.py
+++ setup.py
@@ -1,26 +1,37 @@
-from setuptools import setup
+# from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
+# with open("README.md", "r", encoding="utf-8") as f:
+#     long_description = f.read()
 
-## edit below variables as per your requirements -
-REPO_NAME = "Movie_Recommender_System_Using_Machine_Learning"
-AUTHOR_USER_NAME = "ravi0dubey"
-SRC_REPO = "src"
-LIST_OF_REQUIREMENTS = ['streamlit']
+# ## edit below variables as per your requirements -
+# REPO_NAME = "Movie_Recommender_System_Using_Machine_Learning"
+# AUTHOR_USER_NAME = "ravi0dubey"
+# SRC_REPO = "src"
+# LIST_OF_REQUIREMENTS = ['streamlit']
 
+
+# setup(
+#     name=SRC_REPO,
+#     version="0.0.1",
+#     author=AUTHOR_USER_NAME,
+#     author_email="ravidatascience4@gmail.com",
+#     description="A small package for Movie Recommender System",
+#     long_description=long_description,
+#     long_description_content_type="text/markdown",
+#     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+#     packages=[SRC_REPO],
+#     license="MIT",
+#     python_requires=">=3.7",
+#     install_requires=LIST_OF_REQUIREMENTS
+# )
+
+from setuptools import find_packages, setup
 
 setup(
-    name=SRC_REPO,
+    name="catdog",
     version="0.0.1",
-    author=AUTHOR_USER_NAME,
-    author_email="ravidatascience4@gmail.com",
-    description="A small package for Movie Recommender System",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    packages=[SRC_REPO],
-    license="MIT",
-    python_requires=">=3.7",
-    install_requires=LIST_OF_REQUIREMENTS
+    author="Ravi Dubey",
+    author_email="Ravi0dubey@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
 )
